306fe73e136a5e7d7dd88c6190dc8f2b5efe6b34
Sören Kuklau <kuklau@trenz.de>
2020-12-05 12:23:24 +0100

    day 1 complete

--- day-1/Program.cs
+++ day-1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,6 +14,12 @@ namespace day_1
 
             var ints = lines.Select(s => int.Parse(s));
 
+            TwoInputs(ints);
+            ThreeInputs(ints);
+        }
+
+        private static void TwoInputs(IEnumerable<int> ints)
+        {
             int resultA = -1, resultB = -1;
 
             foreach (var intA in ints)
@@ -31,6 +38,33 @@ namespace day_1
         after:
             {
                 Console.WriteLine($"{resultA} * {resultB} = {resultA * resultB}");
+            }
+        }
+
+        private static void ThreeInputs(IEnumerable<int> ints)
+        {
+            int resultA = -1, resultB = -1, resultC = -1;
+
+            foreach (var intA in ints)
+            {
+                foreach (var intB in ints)
+                {
+                    foreach (var intC in ints)
+                    {
+                        if (intA + intB + intC == 2020)
+                        {
+                            resultA = intA;
+                            resultB = intB;
+                            resultC = intC;
+                            goto after;
+                        }
+                    }
+                }
+            }
+
+        after:
+            {
+                Console.WriteLine($"{resultA} * {resultB} * {resultC} = {resultA * resultB * resultC}");
             }
         }
     }
