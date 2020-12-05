ff7195e841bd3785b86466151a6189ee9229e540
Sören Kuklau <kuklau@trenz.de>
2020-12-05 12:20:33 +0100

    day 1, part 1

---
+++ day-1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace day_1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var lines = await File.ReadAllLinesAsync("input.txt");
+
+            var ints = lines.Select(s => int.Parse(s));
+
+            int resultA = -1, resultB = -1;
+
+            foreach (var intA in ints)
+            {
+                foreach (var intB in ints)
+                {
+                    if (intA + intB == 2020)
+                    {
+                        resultA = intA;
+                        resultB = intB;
+                        goto after;
+                    }
+                }
+            }
+
+        after:
+            {
+                Console.WriteLine($"{resultA} * {resultB} = {resultA * resultB}");
+            }
+        }
+    }
+}
