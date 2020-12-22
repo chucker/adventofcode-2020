3370d0dec6227da96da7d55c4b76e9556474d5b0
Sören Kuklau <kuklau@trenz.de>
2020-12-22 21:11:45 +0100

    starting day 3; buggy

---
+++ day-3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace day_3
+{
+    class Program
+    {
+        private const char TreeChar = '#';
+
+        static async Task Main(string[] args)
+        {
+            var lines = await File.ReadAllLinesAsync("input.txt");
+
+            var position = (X: 0, Y: 0);
+
+            int trees = 0;
+
+            while (position.Y <= lines.Length)
+            {
+                // BUG: need to wrap around horizontally
+
+                if (lines[position.Y][position.X] == TreeChar)
+                    trees++;
+
+                position = (position.X + 3, position.Y + 1);
+            }
+
+            Console.WriteLine(trees);
+        }
+    }
+}
