0b716662e28ab0642182056a347a8c548ea53e20
Sören Kuklau <kuklau@trenz.de>
2020-12-22 20:49:58 +0100

    day 2, part 1

---
+++ day-2/PasswordWithPolicy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace day_2
+{
+    public class PasswordWithPolicy
+    {
+        public int MinOccurrences { get; init; }
+        public int MaxOccurrences { get; init; }
+        public char Char { get; init; }
+        public string Password { get; init; }
+
+        public static PasswordWithPolicy FromGroupCollection(GroupCollection groupCollection)
+        {
+            return new PasswordWithPolicy
+            {
+                MinOccurrences = int.Parse(groupCollection["Min"].Value),
+                MaxOccurrences = int.Parse(groupCollection["Max"].Value),
+                Char = groupCollection["Char"].Value[0],
+                Password = groupCollection["Password"].Value
+            };
+        }
+
+        public bool PasswordIsValid()
+        {
+            int count=0;
+
+            foreach (var item in Password)
+            {
+                if (item == Char)
+                    count++;
+
+                if (count > MaxOccurrences)
+                    return false;
+            }
+
+            if (count < MinOccurrences)
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ day-2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace day_2
+{
+    class Program
+    {
+        const string Regex = @"(?<Min>\d+)-(?<Max>\d+) (?<Char>\w): (?<Password>\w+)";
+
+        static async Task Main(string[] args)
+        {
+            var passwordsWithPolicies = new List<PasswordWithPolicy>();
+
+            foreach (var item in await File.ReadAllLinesAsync("input.txt"))
+            {
+                var match = System.Text.RegularExpressions.Regex.Match(item, Regex);
+
+                passwordsWithPolicies.Add(PasswordWithPolicy.FromGroupCollection(match.Groups));
+            }
+
+            Console.WriteLine(passwordsWithPolicies.Where(pwp => pwp.PasswordIsValid()).Count());
+        }
+    }
+}
