8b36a653f7a8b54460b9a6a1f1112b4e0f9ecb9c
Sören Kuklau <kuklau@trenz.de>
2020-12-27 21:26:27 +0100

    day 7, part 2

--- day-7/Program.cs
+++ day-7/Program.cs
@@ -10,22 +10,31 @@ namespace day_7
 {
     class Program
     {
+        private const string QueriedRule = "shiny gold bag";
+
         static async Task Main(string[] args)
         {
-            foreach (var filename in new[] { "sample-input.txt", "input.txt" })
+            foreach (var filename in Directory.EnumerateFiles(".", "*.txt"))
             {
                 var rules = await ReadAndParseRules(filename);
 
-                int count = 0;
+                    int count = 0;
 
-                foreach (var item in rules)
-                {
-                    if (item.Key == "shiny gold bag")
-                        continue;
+                    foreach (var item in rules)
+                    {
+                        if (item.Key == QueriedRule)
+                            continue;
 
-                    if (item.Value.DoesRecursivelyContainOtherBagType("shiny gold bag", rules))
-                        count++;
-                }
+                        if (item.Value.DoesRecursivelyContainOtherBagType(QueriedRule, rules))
+                            count++;
+                    }
+
+                    Console.WriteLine($"File {filename}: {QueriedRule}s are indirectly contained by {count} other bags");
+
+                int containedBags = 0;
+                rules[QueriedRule].SumContainedBagsRecursively(ref containedBags, rules);
+
+                Console.WriteLine($"File {filename}: {QueriedRule}s ultimately contain {containedBags} other bags");
             }
         }
 
@@ -106,6 +115,24 @@ namespace day_7
             }
 
             return false;
+        }
+
+        public void SumContainedBagsRecursively(ref int totalCount, Dictionary<string, BagRule> allRules)
+        {
+            foreach (var item in ContainedBagRules)
+            {
+                //Console.WriteLine($"{this.BagType} contains {item.BagType}; adding {item.Amount} of those");
+
+                for (int i = 0; i < item.Amount; i++)
+                {
+                    totalCount++;
+
+                    if (!allRules.TryGetValue(item.BagType, out var containedRule))
+                        throw new KeyNotFoundException($"Couldn't find rule for {item}");
+
+                    containedRule.SumContainedBagsRecursively(ref totalCount, allRules);
+                }
+            }
         }
     }
 
